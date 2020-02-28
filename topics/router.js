cf2dcd53341be1253624da4a72da4fb5e780a4db
debauchery1st <trevor.j.martin@gmail.com>
2020-02-27 23:17:24 -0500

    model, connection

--- topics/router.js
+++ topics/router.js
@@ -1,9 +1,13 @@
+const topics = require("./model");
 const router = require("express").Router();
 
-router.use("/", (req, res) => {
+router.get("/", (req, res) => {
   res.status(200).json({
     router: "topics"
   });
 });
 
+router.get("/all", (req, res) => {
+  topics.all().then(t => res.status(200).json(t));
+});
 module.exports = router;
