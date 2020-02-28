48d08fbb709a0ec0a9cb0c4b37cb146c7be1bd72
debauchery1st <trevor.j.martin@gmail.com>
2020-02-28 00:29:06 -0500

    update, delete

--- topics/model.js
+++ topics/model.js
@@ -1,22 +1,45 @@
 const db = require("../database/connection.js");
 
+const topics = () => db("topics");
+
 module.exports = {
   all,
-  add
+  add,
+  updateTopic,
+  removeTopic
 };
 
 function all() {
-  return db("topics");
+  return topics();
 }
 
 function findById(id) {
-  return db("topics")
+  return topics()
     .where({ id })
     .first();
 }
 
 function add(topic) {
-  return db("topics")
+  return topics()
     .insert(topic, "id")
     .then(([id]) => findById(id));
+}
+
+function updateTopic(changes, id) {
+  if (!id) {
+    return { error: "id required" };
+  }
+  return topics()
+    .update(changes)
+    .where({ id })
+    .then(() => findById(id));
+}
+
+function removeTopic(id) {
+  return findById(id).then(was => {
+    return topics()
+      .del()
+      .where({ id })
+      .then(() => was);
+  });
 }
--- topics/router.js
+++ topics/router.js
@@ -13,8 +13,19 @@ router.get("/all", (req, res) => {
 
 router.post("/", (req, res) => {
   req.body.name && req.body.name.length > 0
-    ? topics.add(req.body).then(ret => res.status(201).json(ret))
+    ? topics
+        .add(req.body)
+        .then(ret => res.status(201).json(ret))
+        .catch(err => res.status(400).json({ error: err.detail }))
     : res.status(400).json({ error: "name required" });
+});
+
+router.delete("/:id", (req, res) => {
+  req.params.id
+    ? topics
+        .removeTopic(req.params.id)
+        .then(was => res.status(200).json({ deleted: was }))
+    : res.status(400).json({ error: "?" });
 });
 
 module.exports = router;
